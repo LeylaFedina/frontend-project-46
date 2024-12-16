b62d43c6cf0873f419245e326aa718644c2342b9
Leyla Fedina <leyla_ismailova_b@mail.ru>
2024-12-16 22:34:44 +0300

    поправил отступи, правки для eslint

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ const allUniqKeys = _.union(_.keys(obj1), _.keys(obj2));
 allUniqKeys.forEach((key) => {
 if (_.has(obj1, key) && _.has(obj2, key)) {
  if (_.isEqual(obj1[key], obj2[key])) {
-//Глубокое сравнение: _.isEqual позволяет сравнивать не только простые значения, но и вложенные объекты и массивы
+//Глубокое сравнение: _.isEqual позволяетсравнивать
+не только простые значения, но и вложенные объекты и массивы
   result[key] = 'unchanged';
  } else {
   result[key] = 'changed';
@@ -32,41 +33,41 @@ import _ from 'lodash';
 import { JSONparser, YMLparser } from './parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
-    let result = '';
-    let file1;
-    let file2;
+  let result = '';
+  let file1;
+  let file2;
 
-    const getFormat = (filepath) => filepath.split('.')[1];
+  const getFormat = (filepath) => filepath.split('.')[1];
 
-    if (getFormat(filepath1) === 'json') {
-        file1 = JSONparser(filepath1);
-        file2 = JSONparser(filepath2);
-    } else if (getFormat(filepath1) === 'yml') {
-        file1 = YMLparser(filepath1);
-        file2 = YMLparser(filepath2);
+  if (getFormat(filepath1) === 'json') {
+    file1 = JSONparser(filepath1);
+    file2 = JSONparser(filepath2);
+  } else if (getFormat(filepath1) === 'yml') {
+    file1 = YMLparser(filepath1);
+    file2 = YMLparser(filepath2);
+  }
+
+  const sortedKeys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
+  sortedKeys.map((key) => {
+    if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
+      if (file1[key] === file2[key]) {
+        result += `    ${key}: ${file1[key]}\n`;
+      } else {
+        result += `  - ${key}: ${file1[key]}\n`;
+        result += `  + ${key}: ${file2[key]}\n`;
+      }
     }
 
-    const sortedKeys = _.sortBy(_.union(Object.keys(file1), Object.keys(file2)));
-    sortedKeys.map((key) => {
-        if (Object.hasOwn(file1, key) && Object.hasOwn(file2, key)) {
-            if (file1[key] === file2[key]) {
-                result += `    ${key}: ${file1[key]}\n`;
-            } else {
-                result += `  - ${key}: ${file1[key]}\n`;
-                result += `  + ${key}: ${file2[key]}\n`;
-            }
-        }
+    if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
+      result += `  - ${key}: ${file1[key]}\n`;
+    }
 
-        if (Object.hasOwn(file1, key) && !Object.hasOwn(file2, key)) {
-            result += `  - ${key}: ${file1[key]}\n`;
-        }
-
-        if (Object.hasOwn(file2, key) && !Object.hasOwn(file1, key)) {
-            result += `  + ${key}: ${file2[key]}\n`;
-        }
-        return result;
-    });
-    return `{\n  ${result.trim()}\n}`;
+    if (Object.hasOwn(file2, key) && !Object.hasOwn(file1, key)) {
+      result += `  + ${key}: ${file2[key]}\n`;
+    }
+    return result;
+  });
+  return `{\n  ${result.trim()}\n}`;
 };
 
 export default genDiff;
