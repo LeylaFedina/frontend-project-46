fb412b4a26cec5bce7820dc69da31c3ca323fa9f
Leyla Fedina <leyla_ismailova_b@mail.ru>
2024-12-17 20:45:36 +0300

    Исправиление ошибок eslint

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-/*
-import _ from 'lodash';
-
-function getDiff(obj1, obj2) {
-const result = {};
-
-// Получаем все уникальные ключи из обоих объектов
-const allUniqKeys = _.union(_.keys(obj1), _.keys(obj2));
-
-allUniqKeys.forEach((key) => {
-if (_.has(obj1, key) && _.has(obj2, key)) {
- if (_.isEqual(obj1[key], obj2[key])) {
-//Глубокое сравнение: _.isEqual позволяетсравнивать
-не только простые значения, но и вложенные объекты и массивы
-  result[key] = 'unchanged';
- } else {
-  result[key] = 'changed';
- }
-} else if (_.has(obj1, key)) {
- result[key] = 'deleted';
-} else if (_.has(obj2, key)) {
- result[key] = 'added';
-}
-});
-
-return result;
-}
-
-export default getDiff;
-*/
-
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 import { cwd } from 'process';
@@ -51,7 +20,7 @@ const genDiff = (filepath1, filepath2) => {
   const file2 = parseFile(getExtension(filepath2), readfile(filepath2));
 
   const diffTree = buildDiff(file1, file2);
-  console.log(formatter(diffTree));
+  return formatter(diffTree);
 };
 
 export default genDiff;
