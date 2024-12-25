f39ab5c5f72bf7198be58e32f9f1946407dd66da
Leyla Fedina <leyla_ismailova_b@mail.ru>
2024-12-25 20:41:06 +0300

    подкорректировал для eslint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,7 +9,6 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-
 const listOfTests = [
   'yml',
   'json',
@@ -26,8 +25,6 @@ describe('gendiff', () => {
     expect(genDiff(filepath1, filepath2, 'json')).toEqual(readFile('resultJSON.txt'));
   });
 });
-
-
 
 /*
 // Одинокие тестт
@@ -65,10 +62,14 @@ test('JSON gendiff test', () => {
 
 // Вариант без Вывода имени
 test.each([
-  {file1: getFixturePath('file1.json'), file2: getFixturePath('file2.json'), expected: readFile('resultTree.txt'), format: '', testName: 'main JSON test'},
-  {file1: getFixturePath('file1.yml'), file2: getFixturePath('file2.yml'), expected: readFile('resultTree.txt'), format: '', testName: 'main YML test'},
-  {file1: getFixturePath('file1.json'), file2: getFixturePath('file2.json'), expected: readFile('resultPlain.txt'), format: 'plain', testName: 'Plain gendiff test'},
-  {file1: getFixturePath('file1.json'), file2: getFixturePath('file2.json'), expected: readFile('resultJSON.txt'), format: 'json', testName: 'JSON gendiff test'},
+  {file1: getFixturePath('file1.json'), file2: getFixturePath('file2.json'),
+  expected: readFile('resultTree.txt'), format: '', testName: 'main JSON test'},
+  {file1: getFixturePath('file1.yml'), file2: getFixturePath('file2.yml'),
+  expected: readFile('resultTree.txt'), format: '', testName: 'main YML test'},
+  {file1: getFixturePath('file1.json'), file2: getFixturePath('file2.json'),
+   expected: readFile('resultPlain.txt'), format: 'plain', testName: 'Plain gendiff test'},
+  {file1: getFixturePath('file1.json'), file2: getFixturePath('file2.json'),
+  expected: readFile('resultJSON.txt'), format: 'json', testName: 'JSON gendiff test'},
 ])('', ({file1, file2, expected, testName}) => {
   expect(genDiff(file1, file2)).toEqual(expected);
 });
